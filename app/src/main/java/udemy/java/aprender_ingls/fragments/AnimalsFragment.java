61ca7b304c5fa765ec3366ff28e1c900ad7ea3bc
Sérgio <a18074@alunos.ipca.pt>
2022-10-03 16:39:56 +0100

    complete app;

--- app/src/main/java/udemy/java/aprender_ingls/fragments/AnimalsFragment.java
+++ app/src/main/java/udemy/java/aprender_ingls/fragments/AnimalsFragment.java
@@ -1,18 +1,23 @@
 package udemy.java.aprender_ingls.fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.viewmodel.CreationExtras;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import udemy.java.aprender_ingls.R;
 
-public class AnimalsFragment extends Fragment {
+public class AnimalsFragment extends Fragment implements View.OnClickListener{
 
-
+    private MediaPlayer mediaPlayer;
 
     public AnimalsFragment() {
         // Required empty public constructor
@@ -28,6 +33,76 @@ public class AnimalsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animals, container, false);
+       View view = inflater.inflate(R.layout.fragment_animals, container, false);
+
+        ImageView imageViewCao = view.findViewById(R.id.imageView_animalCao);
+        ImageView imageViewGato = view.findViewById(R.id.imageView_animalGato);
+        ImageView imageViewLeao = view.findViewById(R.id.imageView_animalLeao);
+        ImageView imageViewVaca = view.findViewById(R.id.imageView_animalVaca);
+        ImageView imageViewMacaco = view.findViewById(R.id.imageView_animalMacaco);
+        ImageView imageViewOvelha = view.findViewById(R.id.imageView_animalOvelha);
+
+        imageViewCao.setOnClickListener(this);
+        imageViewGato.setOnClickListener(this);
+        imageViewLeao.setOnClickListener(this);
+        imageViewVaca.setOnClickListener(this);
+        imageViewMacaco.setOnClickListener(this);
+        imageViewOvelha.setOnClickListener(this);
+
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Log.i("Elemento clicado","Item" + view.getId() );
+
+        switch ( view.getId()) {
+            case R.id.imageView_animalCao:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.dog);
+                torcarSons();
+                break;
+
+            case R.id.imageView_animalGato:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.cat);
+                torcarSons();
+                break;
+
+            case R.id.imageView_animalLeao:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.lion);
+                torcarSons();
+                break;
+
+            case R.id.imageView_animalMacaco:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.monkey);
+                torcarSons();
+                break;
+
+            case R.id.imageView_animalVaca:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.cow);
+                torcarSons();
+                break;
+
+            case R.id.imageView_animalOvelha:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.sheep);
+                torcarSons();
+                break;
+
+        }
+    }
+
+    private void torcarSons() {
+        if ( mediaPlayer != null){
+         mediaPlayer.start();
+
+         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+             @Override
+             public void onCompletion(MediaPlayer mp) {
+                 mp.release();
+             }
+         });
+        }
     }
 }
--- app/src/main/java/udemy/java/aprender_ingls/fragments/NumbersFragment.java
+++ app/src/main/java/udemy/java/aprender_ingls/fragments/NumbersFragment.java
@@ -1,5 +1,6 @@
 package udemy.java.aprender_ingls.fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import udemy.java.aprender_ingls.R;
 
 
-public class NumbersFragment extends Fragment {
+public class NumbersFragment extends Fragment implements View.OnClickListener {
 
-
+    private MediaPlayer mediaPlayer;
 
     public NumbersFragment() {
         // Required empty public constructor
@@ -28,6 +30,71 @@ public class NumbersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numbers, container, false);
+        View view = inflater.inflate(R.layout.fragment_numbers, container, false);
+
+        ImageView imageViewUm = view.findViewById(R.id.imageView_numberUm);
+        ImageView imageViewDois = view.findViewById(R.id.imageView_numberDois);
+        ImageView imageViewTres = view.findViewById(R.id.imageView_numberTres);
+        ImageView imageViewQuatro = view.findViewById(R.id.imageView_numberQuartro);
+        ImageView imageViewCinco = view.findViewById(R.id.imageView_numberCinco);
+        ImageView imageViewSeis = view.findViewById(R.id.imageView_numberSeis);
+
+        imageViewUm.setOnClickListener(this);
+        imageViewDois.setOnClickListener(this);
+        imageViewTres.setOnClickListener(this);
+        imageViewQuatro.setOnClickListener(this);
+        imageViewCinco.setOnClickListener(this);
+        imageViewSeis.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageView_numberUm:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.one);
+                tocarSonsNumeros();
+                break;
+
+            case R.id.imageView_numberDois:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.two);
+                tocarSonsNumeros();
+                break;
+
+            case R.id.imageView_numberTres:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.three);
+                tocarSonsNumeros();
+                break;
+
+            case R.id.imageView_numberQuartro:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.four);
+                tocarSonsNumeros();
+                break;
+
+            case R.id.imageView_numberCinco:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.five);
+                tocarSonsNumeros();
+                break;
+
+            case R.id.imageView_numberSeis:
+                mediaPlayer = MediaPlayer.create(getContext(), R.raw.six);
+                tocarSonsNumeros();
+                break;
+
+        }
+    }
+
+    private void tocarSonsNumeros() {
+        if (mediaPlayer != null){
+         mediaPlayer.start();
+
+         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+             @Override
+             public void onCompletion(MediaPlayer mp) {
+                 mp.release();
+             }
+         });
+        }
     }
 }
